835e5d9162002fd4a174cdd644431b6554578cf7
ausiemass1 <ausiemass@gmail.com>
2025-12-15 13:30:02 +1300

    create product CRUD functionality

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const adminDashboadController = require('../controllers/adminDashboardController')
+const productCntroller = require('../controllers/productController')
 const router = express.Router();
 
 router.get('/dashboard', adminDashboadController.adminDashboard)
+router.get('/products', productCntroller.displayProducts )
 
 module.exports = router;
